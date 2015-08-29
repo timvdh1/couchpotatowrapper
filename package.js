bedfad622d070aec18d717768b786c68d12fc36d
Mikhail Delport <mikhail@delport.ca>
2015-08-29 13:34:47 -0400

    Package name fix

--- package.js
+++ package.js
@@ -27,6 +27,6 @@ Package.onUse(function(api) {
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('couchpotatowrapper');
+  api.use('lokenx:couchpotatowrapper');
   api.addFiles('couchpotatowrapper-tests.js');
 });
