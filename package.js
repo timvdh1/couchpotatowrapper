c08690634489b40901349cb2410823fbcde9fbe0
Mikhail Delport <mikhail@delport.ca>
2015-08-29 13:32:39 -0400

    GitHub URL

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Wrapper for the Couch Potato API',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/lokenx/couchpotatowrapper',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
