a0b5624da9e3fab8b77bed70689db405d7bc600a
Tim van den Hanenberg <tim.vd.hanenberg@gmail.com>
2015-12-26 14:26:04 +0100

    no message

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'lokenx:couchpotatowrapper',
-  version: '0.0.7',
+  name: 'timvdh1:couchpotatowrapper',
+  version: '0.0.1',
   // Brief, one-line summary of the package.
   summary: 'Wrapper for the Couch Potato API',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/lokenx/couchpotatowrapper',
+  git: 'https://github.com/timvdh1/couchpotatowrapper',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
